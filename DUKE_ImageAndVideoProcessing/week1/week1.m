1f7a738b27a3c19bc0c1867dd4ce0d1fd12088ad
YoungGer <771657815@qq.com>
2016-01-12 16:56:55 +0800

    杜克图像处理还有华盛顿的LASSO

--- DUKE_ImageAndVideoProcessing/week1/week1.m
+++ DUKE_ImageAndVideoProcessing/week1/week1.m
@@ -46,3 +46,8 @@ figure,imshow(r45),figure,imshow(r90);
 
 %% replace all corresponding 9 pixels by their average
 %  5*5 7*7
+fun = @(block_struct) uint8( mean2(block_struct.data(:))  * ones(block_struct.blockSize) );
+block_avg_3 = blockproc(gray_im,[3 3],fun);
+block_avg_5 = blockproc(gray_im,[5 5],fun);
+block_avg_7 = blockproc(gray_im,[7 7],fun);
+figure,imshow(gray_im),figure,imshow(block_avg_3),figure,imshow(block_avg_5),figure,imshow(block_avg_7);
