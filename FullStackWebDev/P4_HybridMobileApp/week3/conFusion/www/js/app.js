b1d309ef92cff05bf36f3816736289fd15cd6887
YoungGy <771657815@qq.com>
2016-02-09 10:39:30 +0800

    angular ui-router and resolve

---
+++ FullStackWebDev/P4_HybridMobileApp/week3/conFusion/www/js/app.js
@@ -0,0 +1,108 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.services'])
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/sidebar.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'mainContent': {
+        templateUrl: 'templates/home.html',
+        controller: 'IndexController'
+      }
+    }
+  })
+
+  .state('app.aboutus', {
+      url: '/aboutus',
+      views: {
+        'mainContent': {
+          templateUrl: 'templates/aboutus.html',
+          controller: 'AboutController'
+        }
+      }
+    })
+
+    .state('app.contactus', {
+      url: '/contactus',
+      views: {
+        'mainContent': {
+          templateUrl: 'templates/contactus.html'
+        }
+      }
+    })
+
+
+    .state('app.menu', {
+      url: '/menu',
+      views: {
+        'mainContent': {
+          templateUrl: 'templates/menu.html',
+          controller: 'MenuController'
+        }
+      }
+    })
+
+    .state('app.favorites', {
+      url: '/favorites',
+      views: {
+        'mainContent': {
+          templateUrl: 'templates/favorites.html',
+            controller:'FavoritesController',
+          resolve: {
+              dishes:  ['menuFactory', function(menuFactory){
+                return menuFactory.query();
+              }],
+              favorites: ['favoriteFactory', function(favoriteFactory) {
+                  return favoriteFactory.getFavorites();
+              }]
+          }
+        }
+      }
+    })
+
+  .state('app.dishdetails', {
+    url: '/menu/:id',
+    views: {
+      'mainContent': {
+        templateUrl: 'templates/dishdetail.html',
+        controller: 'DishDetailController',
+        resolve: {
+            dish: ['$stateParams','menuFactory', function($stateParams, menuFactory){
+                return menuFactory.get({id:parseInt($stateParams.id, 10)});
+            }]
+        }
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+});
