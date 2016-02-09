2261c0df8f3e8abb0ca194349e592d775b62fb1d
YoungGy <771657815@qq.com>
2016-02-09 10:56:45 +0800

    angular增加了localStore

--- FullStackWebDev/P4_HybridMobileApp/week3/conFusion/www/js/services.js
+++ FullStackWebDev/P4_HybridMobileApp/week3/conFusion/www/js/services.js
@@ -33,6 +33,23 @@ angular.module('conFusion.services', ['ngResource'])
                         
         // }])
 
+        .factory('$localStorage', ['$window', function($window) {
+          return {
+            store: function(key, value) {
+              $window.localStorage[key] = value;
+            },
+            get: function(key, defaultValue) {
+              return $window.localStorage[key] || defaultValue;
+            },
+            storeObject: function(key, value) {
+              $window.localStorage[key] = JSON.stringify(value);
+            },
+            getObject: function(key,defaultValue) {
+              return JSON.parse($window.localStorage[key] || defaultValue);
+            }
+          }
+        }])
+
         .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
                     return $resource(baseURL + "dishes/:id", null, {
